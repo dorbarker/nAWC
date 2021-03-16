7405aed73a12cc942b070836f08bda8f54db5a53
chapb <brennan@chapmanb.com>
2021-03-15 21:03:37 -0400

    Remove personalization

--- nawc.R
+++ nawc.R
@@ -11,7 +11,7 @@ suppressPackageStartupMessages(
         usePackage("reshape2")
         usePackage("optparse")
         
-        source("D:/Dropbox (Personal)/GitHub/nAWC/wallace.R")
+        source("wallace.R")
     }
 )
 
